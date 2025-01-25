c2d853ee13fa514dbfe3724ff8a61729c660ac6d
mustafa-bircan <msbrtfcn@gmail.com>
2025-01-25 15:42:04 +0300

    HomePage and OrderPage Components Revized
    -HomePage was updated and revised.
    -OrderPage was created, but its content is not yet prepared.
    -CSS files for HomePage were added and styled.

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './HomePage.css'
+
+function HomePage() {
+  return (
+    
+    <>
+    <div className="hero-section">
+      <div className="content">
+        <img src='../images/iteration-1-images/logo.svg' />
+        <h2 className="subtitle">KOD ACIKTIRIR, PİZZA DOYURUR</h2>
+        <button className="btn">Acıktım</button>
+      </div>
+    </div>
+  </>
+  
+  )
+}
+
+export default HomePage
