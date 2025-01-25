c945d88c3c539cd516ab942157561ea1ff6e13ee
mustafa-bircan <msbrtfcn@gmail.com>
2025-01-26 01:10:22 +0300

    HomePage.jsx and HomePage.css files have been completed.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import './HomePage.css'
+import { Link, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 
 function HomePage() {
-  return (
-    
-    <>
-    <div className="hero-section">
-      <div className="content">
-        <img src='../images/iteration-1-images/logo.svg' />
-        <h2 className="subtitle">KOD ACIKTIRIR, PİZZA DOYURUR</h2>
-        <button className="btn">Acıktım</button>
-      </div>
-    </div>
-  </>
-  
-  )
+    return (
+
+        <>
+            <div className="home-section">
+                <div className="home-content">
+                    <img src='../images/iteration-1-images/logo.svg' />
+                    <h2 className="home-subtitle">KOD ACIKTIRIR, PİZZA DOYURUR</h2>
+                    <Link to='/OrderPage'>
+                        <button className="btn">Acıktım</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+
+    )
 }
 
 export default HomePage
