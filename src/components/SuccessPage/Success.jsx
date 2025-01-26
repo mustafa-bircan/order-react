276e97ec6a8bdca16703b16af162e9b48ddc9522
mustafa-bircan <msbrtfcn@gmail.com>
2025-01-26 06:02:52 +0300

    Success.jsx and Success.css files were created.
    If the information on the form is filled in correctly and completely, you are directed to the Success page.

---
+++ src/components/SuccessPage/Success.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Success.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+function Success() {
+  return (
+    <>
+                <div className="home-section">
+                    <div className="home-content">
+                        <img src='../sample-interfaces/iteration-1/Success.png' />
+                    </div>
+                </div>
+            </>
+  );
+}
+
+export default Success;
